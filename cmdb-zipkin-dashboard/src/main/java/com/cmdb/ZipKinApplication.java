e9cd5d51baf8de24a450d17d7c289d0ad0807232
shibaohua <286316993@qq.com>
2019-05-22 14:53:47 +0800

    修改配置文件

--- cmdb-zipkin-dashboard/src/main/java/com/cmdb/ZipKinApplication.java
+++ cmdb-zipkin-dashboard/src/main/java/com/cmdb/ZipKinApplication.java
@@ -2,6 +2,7 @@ package com.cmdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import zipkin.server.EnableZipkinServer;
 
@@ -10,6 +11,7 @@ import zipkin.server.EnableZipkinServer;
  *
  */
 @SpringBootApplication
+@EnableEurekaClient
 @EnableZipkinServer
 public class ZipKinApplication {
 	public static void main(String[] args) {
