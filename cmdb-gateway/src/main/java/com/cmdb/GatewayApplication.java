63a8a601565cef870914046d01ecf320d0191743
shibaohua <286316993@qq.com>
2019-06-06 09:36:51 +0800

    增加gateway子模块

---
+++ cmdb-gateway/src/main/java/com/cmdb/GatewayApplication.java
@@ -0,0 +1,31 @@
+package com.cmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class GatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+//	@Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//       return builder.routes()
+//        .route(p -> p
+//            .path("/hello")
+//            .filters(f -> f.addRequestHeader("Hello", "World"))
+//            .uri("http://10.1.35.15:8081"))
+//        .build();
+//    }
+}
